e880309f936e4840a63e43f9887e3608d7ef8ab7
THLOgN <102423409+THLOgN@users.noreply.github.com>
2024-12-15 08:56:28 +0700

    `Update getMessage function to filter messages by senderId`

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -2,8 +2,6 @@ import User from "../models/user.model";
 import Messgage from "../models/message.model.js";
 
 
-
-
 export const getUsersForSidebar = async (req, res) => {
 
 };
@@ -14,12 +12,46 @@ export const getMessage = async(req, res ) => {
         const senderId = req.user._id;
         
         const messages = await Message.find({
-            $or:{
-                {send}
-            }
-        })
+            $or:[
+                {senderId:myId, receiverId:userToChatId},
+                {senderId:userToChatId, receiverId:myId}
+
+            ]  
+        });
+        res.status(200).json(messages);
+    } catch (error) {
+        console.log("Error in getMessages controller: ", error.message);
+        res.status(500).json({message: "Internal Server Error"});
+    }
+};
+
+export const sendMessage = async (req, res) => {
+    try {
+        const {text, image} = req.body;
+        const {id: receiverId} = req.params;
+        const senderId = req.user._id;
+
+        let imageUrl;
+        if(image){
+            // Upload base 64 image to cloudinary
+            const uploadedResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadedResponse.secure_url;
+
+        }
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl,
+        });
+
+        await newMessage.save();
+        //
+
+        res.status(201).json(newMessage);
 
     } catch (error) {
-        
+         console.log("Error in sendMessage controller: ", error.message);
+         res.status(500).json({message: "Internal Server Error"});
     }
-}
+};
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,13 @@
 import { express } from 'express';
 import { protectRoute } from '../middleware/auth.middleware';
-im
+import { getMessage, getUsersForSidebar, sendMessage } from '../controllers/message.controller.js';
 
 const router = express.Router();
 
-router.get("/users", protectRoute, getUsersForSidebar )
+router.get("/users", protectRoute, getUsersForSidebar );
+
+router.get("/:id", protectRoute, getMessage);
+
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
